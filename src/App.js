9f347eece5bc556e633a8ea2def6f7ce12c3440c
JuanCruzEtchart <etchart.juancruz@gmail.com>
2023-02-09 02:39:38 -0300

    Fetch de creación de usuarios y bycriptjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import bcrypt, { genSaltSync } from "bcryptjs";
 
 function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const salt = bcrypt.genSaltSync(12);
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let encryptedPassword = bcrypt.hashSync(password, salt);
+      let response = await fetch("http://localhost:3000/api/users/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: name,
+          password: encryptedPassword,
+        }),
+      });
+      if (response.status === 200) {
+        setName("");
+        setPassword("");
+        setMessage("User created successfully");
+      } else {
+        setMessage("Some error occured");
+      }
+      return response.json();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Create</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
     </div>
   );
 }
