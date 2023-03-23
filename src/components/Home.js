82cacfefcc479e9938d92bcd12a1df8d626fa625
Juan Cruz Etchart <etchart.juancruz@gmail.com>
2023-03-22 23:17:47 -0300

    Avance maquetado y estilos de home

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,32 @@ export default function Home() {
         </div>
         <div className="main__data-container">
           <div className="main__vehicle-number">Vehículo 1</div>
+          <div className="main__vehicle-sensor-container">
+            <div className="main__sensor">Puerta:</div>
+            <ul className="main__sensor-data">
+              <li className="main__sensor-type">
+                Estado de la puerta:
+                <div className="main__sensor-status">abierta</div>
+              </li>
+              <li className="main__sensor-type">
+                Tiempo máximo de la puerta abierta:
+                <div className="main__sensor-status">55 min</div>
+              </li>
+            </ul>
+          </div>
+          <div className="main__vehicle-sensor-container">
+            <div className="main__sensor">Velocidad:</div>
+            <ul className="main__sensor-data">
+              <li className="main__sensor-type">
+                Velocidad actual:
+                <div className="main__sensor-status">30km/h</div>
+              </li>
+              <li className="main__sensor-type">
+                Velocidad máxima:
+                <div className="main__sensor-status">80km/h</div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
